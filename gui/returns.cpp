4085d29f48909b13910afe1b8b4817e468a6c650
hugo <ballshugo@yandex.ru>
2025-09-09 21:35:45 +0300

    8

--- gui/returns.cpp
+++ gui/returns.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "returns.hpp"
+
+#include <ftxui/component/event.hpp>
+
+namespace gui {
+
+    using namespace ftxui;
+
+    // --- ReturnValue
+    ReturnValueElement::ReturnValueElement(return_value_t value) : m_value(std::move(value)){
+        make_component();
+    }
+
+    Element ReturnValueElement::OnRender() {
+        return m_element;
+    }
+
+    bool ReturnValueElement::OnEvent(Event event) {
+        return ComponentBase::OnEvent(event);
+    }
+
+    void ReturnValueElement::value(return_value_t value) {
+        m_value = std::move(value);
+    }
+
+    void ReturnValueElement::make_component() {
+        m_element = text(m_value);
+    }
+
+
+    // --- Returns
+    void Returns::make_component() {
+
+    }
+
+}
--- gui/returns.hpp
+++ gui/returns.hpp
@@ -7,10 +7,30 @@
 
 #include "ftxui/component/component_base.hpp"
 #include "../device/device_control.hpp"
+#include <unordered_map>
 
 namespace gui {
 
     using namespace ftxui;
+
+    using return_name_t = std::string;
+    using return_value_t = std::string;
+
+    class ReturnValueElement : public ComponentBase {
+    public:
+        ReturnValueElement(return_value_t value);
+
+        Element OnRender() override;
+        bool OnEvent(Event) override;
+
+        void value(return_value_t value);
+
+    private:
+        return_value_t m_value;
+        Element m_element;
+
+        void make_component();
+    };
 
     class Returns : public ComponentBase, public so_5::agent_t {
 
@@ -21,6 +41,17 @@ namespace gui {
 
     private:
         so_5::mbox_t m_board;
+
+
+        using map_name_to_value_t = std::unordered_map<return_name_t, ReturnValueElement>;
+
+        map_name_to_value_t m_map_name_to_value;
+
+
+
+        void make_component();
+
+
     };
 }
 
